3612e6a0c07568058dffec592cff060422b3a45b
Pedro Sampaio <98555328+sampaiopedroh@users.noreply.github.com>
2025-02-25 21:25:18 -0300

    Add files via upload

---
+++ src/AddTwoNumbers/AddTwoNumbers.java
@@ -0,0 +1,28 @@
+package AddTwoNumbers;
+
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        /*
+        You are given two non-empty linked lists representing two non-negative integers.
+        The digits are stored in reverse order and each of their node contain a single digit.
+        Add the two numbers and return it as a linked list
+
+        You may assume the two numbers do not contain any leading zero, except the number 0 itself
+
+        Example:
+        Input: {2,4,3} + {5,6,4}
+        Output: {7,0,8}
+        Explanation: 342 + 465 = 807;
+        */
+
+
+    }
+
+    public static int passaListaParaInt(int[] lista) {
+        int numero = 0;
+        for (int i = 0; i < lista.length; i++) {
+            int casa = 10 *;
+            numero += lista[i] * casa;
+        }
+    }
+}
